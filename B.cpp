5e4c2dc0989d7e770ce9e615997a8f9eba39f987
JaimeGutierrezM <90988866+JaimeGutierrezM@users.noreply.github.com>
2025-06-27 18:47:00 -0500

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> graph[26];
+bool check[26];
+string result;
+
+
+void dfs(int u) 
+{
+    check[u] = true;
+
+    for (int v : graph[u])
+    {
+        if (!check[v]) dfs(v);
+    }
+        
+    result += (char)(u + 'A');
+}
+
+int main() {
+
+    string s1, s2;
+    cin >> s1;
+    while (cin >> s2 && s2 != "#")
+    {
+        for (int i = 0; i < s1.size() && i < s2.size(); ++i)
+        {
+            if (s1[i] != s2[i])
+            {
+                graph[s1[i] - 'A'].push_back(s2[i] - 'A');
+                break;
+            }
+        }
+            
+        s1 = s2;
+    }
+
+    for (int i = 0; i < 26; ++i)
+    {
+        if (!check[i] && graph[i].size()) dfs(i);
+    }
+        
+    for (int i = result.size() - 1; i >= 0; --i)
+    {
+        cout << result[i];
+    } 
+
+    cout << endl ;
+}
