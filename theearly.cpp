819fe18eb6723784388d06fee0ededc6ec4688d7
JaimeGutierrezM <90988866+JaimeGutierrezM@users.noreply.github.com>
2025-06-29 23:35:45 -0500

    Update theearly.cpp

--- theearly.cpp
+++ theearly.cpp
@@ -1,37 +1,29 @@
+//theearly
 #include <iostream>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 
 inline bool IsPerfect(int p)
 {
+  
     return p == 2 || p == 3 || p == 5 || p == 7 ||
-        p == 13 || p == 17 || p == 19 || p == 31;
+        p == 13 || p == 17;
 }
 
 int main()
 {
     int N;
     cin >> N;
-    cin.ignore();
-
-    string line;
-    getline(cin, line);
-
-    stringstream ss(line);
-    string token;
-    int count = 0;
-
-    while (getline(ss, token, ',') && count < N)
+    
+    char junk;
+    for (int i = 0; i < N; ++i)
     {
-        int p = stoi(token);
+        int p;
+        cin >> p >> junk;
+        
         if (IsPerfect(p))
             cout << "Yes\n";
         else
             cout << "No\n";
-        ++count;
     }
-
-    return 0;
 }
